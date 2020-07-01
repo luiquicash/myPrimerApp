a12054978c4a4d752ecb7ef2fbcec43d0afcd663
Luis Luna <lluna@tekhsoft.com>
2020-07-01 14:09:58 -0400

    Finalizacion de Tarea 1

---
+++ myPrimerApp/myPrimerApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace myPrimerApp
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Clicked(object sender, EventArgs e)
+        {
+            if(txtNom.Text != null)
+            {
+                lblRes.Text = "Bienvenido/a  Sr/a :" + txtNom.Text;
+                txtNom.Text="";
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Favor Introducir un Nombre Antes de Pulsar el Boton", "OK");
+            }
+        }
+    }
+}
